93943d8457215315ee8b77d2f80aa8e2371dd5aa
fst2000 <fst.18.work@gmail.com>
2023-10-31 17:45:59 +0400

    Smooth input

--- Camera3D.gd
+++ Camera3D.gd
@@ -2,9 +2,9 @@ extends Camera3D
 
 @export var _origin: Node3D
 
-var _distance = 5
-var _minHeight = 2
-var _maxHeight = 3
+var _distance = 5.0
+var _minHeight = 2.0
+var _maxHeight = 3.0
 
 func _ready():
 	pass
@@ -13,7 +13,7 @@ func _process(delta):
 	var origPos = _origin.position
 	#var distance = origPos.distance_to(position)
 	var camHeight = position.y - origPos.y
-	var clampedHeight : float = clamp(camHeight, _minHeight, _maxHeight)
+	var clampedHeight = clamp(camHeight, _minHeight, _maxHeight)
 	var lerpHeight = lerp(camHeight, clampedHeight, 0.1)
 	position.y = origPos.y + lerpHeight
 	var camPos = origPos + (origPos.direction_to(position) * _distance)
--- player.gd
+++ player.gd
@@ -3,38 +3,36 @@ extends CharacterBody3D
 @export var _camera: Camera3D
 var moveVelocity = Vector3.ZERO
 var fallVelocity = Vector3.ZERO
+var _lerpInput = Vector3.ZERO
 var _gravity = -10;
 var _jumpForce = 6;
-var _speed = 5
-var isWalking = false
-var isFalling = false
+var _speed = 4
 
 func _ready():
 	pass
 
-#func _process(delta):
-#	if is_on_floor():
-#		if moveVelocity.length() > 0.1:
-#			if !isWalking: _animator.play("Walk")
-#			isWalking = true
-#	else:
-#		if !isFalling: _animator.play("Fall")
-	
+
 func _physics_process(delta):
-	var input = Vector2(
-		Input.get_axis("right", "left"),
-		Input.get_axis("down", "up")).normalized()
 	var isJump = is_on_floor() && Input.is_action_pressed("jump")
+	
+	var input = Vector3(
+		Input.get_axis("right", "left"),
+		0,
+		Input.get_axis("down", "up")).normalized()
+	_lerpInput = lerp(_lerpInput, input, 0.1)
+	
 	var lookRotation = _camera.rotation.y + PI
-	moveVelocity = Vector3(input.x, 0, input.y) * _speed * delta * 50
+	moveVelocity = _lerpInput.rotated(Vector3.UP, lookRotation) * _speed * delta * 50
+	
 	if is_on_floor(): fallVelocity.y = 0
 	else: fallVelocity.y += _gravity * delta
+	
 	if isJump:
 		fallVelocity.y += _jumpForce
 		
-	moveVelocity = moveVelocity.rotated(Vector3.UP, lookRotation)
-	if moveVelocity.length() > 0.01:
+	if moveVelocity.length() > 0.1:
 		var playerRotation = position - moveVelocity
 		look_at(playerRotation)
+	
 	velocity = moveVelocity + fallVelocity
 	move_and_slide()
